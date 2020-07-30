ab0d03addaddd37eadec143c820bc06e33b8286a
Raphael Cardoso <cardosodeoliveir@gmail.com>
2020-07-29 22:32:39 -0300

    Adicionados arquivos de comunicação

---
+++ net/grSim_client.h
@@ -0,0 +1,32 @@
+#ifndef GRSIMCLIENT_H
+#define GRSIMCLIENT_H
+
+#include <QObject>
+#include <QUdpSocket>
+
+#include "pb/command.pb.h"
+#include "pb/common.pb.h"
+#include "pb/packet.pb.h"
+#include "pb/replacement.pb.h"
+
+class GrSim_Client : public QObject
+{
+    Q_OBJECT
+public:
+    explicit GrSim_Client(QObject *parent = 0);
+    void sendCommand(double velX, int id);
+    void sendCommand(double velE, double velD, int id);
+    QHostAddress _addr;
+    quint16 _port;
+
+signals:
+
+public slots:
+    void readyRead();
+
+private:
+    QUdpSocket *socket;
+
+};
+
+#endif // GRSIMCLIENT_H
